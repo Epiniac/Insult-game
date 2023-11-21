f24c8d5d2b183e2c5782cbbbb6b016cd0c5297dc
cognitech <j_chen39@hetic.eu>
2023-11-21 11:14:42 +0100

    functionnal game alpha

--- lib/classes.py
+++ lib/classes.py
@@ -1,4 +1,13 @@
 class Character:
+    insult_block = [
+        "fleshless", "dog food", "can't aim", "scared of cats", "basically a glitched pig",
+        "creepy", "brainless", "bag of rotten flesh", "emergency food", 
+        "can't swim", "skinny tower", "ugly", "gold digger", "gets bitten by its mount",
+        "stupid", "unreliable", "empty pumpkin head", "tall and scary",
+        "Your mom", "dumdass", "little shit", "coward", "airhead", "asshole", "ass-kisser",
+        "bimbo", "bastard", "rat"
+    ]
+
     def __init__(self, name, weaknesses):
         self.name = name
         self.current_insult = ""
@@ -14,18 +23,35 @@ class Character:
     def reset_insult(self):
         self.current_insult = ""
 
-    def calculate_damage(self, opponent):
-        effectiveness = self.calculate_effectiveness(opponent)
-        damage = effectiveness + 4
-        opponent_score_before = opponent.score
-        opponent.score -= damage
-        return damage, opponent_score_before, opponent.score
+    def combo_meter(self):
+        combo = 0
+        for block in self.insult_block:
+            if block in self.current_insult:
+                combo += 1
+        return combo
     
     def effectiveness(self):
         effect = 2
         for weakness in self.weaknesses:
             if weakness in self.current_insult:
-                effect*=2
+                print("\nEmotional Damage !!")
+                effect *= 2
+        return effect
+
+    def calculate_damage(self, player, opponent):
+        effect = self.effectiveness()
+        combo = self.combo_meter()
+        damage = effect + combo
+        opponent_score_before = opponent.score
+        opponent.score -= damage
+        return damage, opponent_score_before, opponent.score
+
+    def effectiveness(self):
+        effect = 2
+        for weakness in self.weaknesses:
+            if weakness in self.current_insult:
+                print("\nEmotional Damage !!")
+                effect *= 2
         return effect
 
 skeleton_weak = ["fleshless", "dog food", "can't aim"]
@@ -46,8 +72,58 @@ piglin = Character("Piglin", piglin_weak)
 golem_weak = ["unreliable", "empty pumpkin head", "tall and scary"]
 golem = Character("Golem",golem_weak)
 
-insult_block = ["fleshless", "dog food", "can't aim", "scared of cats", "basicly a gliched pig",
-                "creepy", "brainless", "bag of rotten flesh", "emergency food", 
-                "can't swim", "skinny tower", "ugly", "gold digger", "gets bitten by its mount",
-                "stupid", "unreliable", "empty pumpkin head", "tall and scary",
-                "Your mom", "dumdass", "little shit", "coward", "airhead", "asshole", "ass-kisser","bimbo", "bastard", "rat"]
+c_list = [skeleton, creeper, zombie, enderman, piglin, golem]
+
+name_list = [skeleton.name,creeper.name,zombie.name,enderman.name,piglin.name,golem.name]
+def main():
+
+    for character in c_list:
+        print(character.name," / ", character.weaknesses)
+
+    player1_input = str(input("\nSelect a character for player one:"))
+
+    while player1_input not in name_list:
+        player1_input = str(input("\nPlease select a valid character! :"))
+
+    lengh = len(c_list)
+    for i in range(lengh):
+        if name_list[i] == player1_input:
+            player1 = c_list[i]
+
+    player2_input = str(input("\nSelect a character for player two:"))
+
+    while player2_input not in name_list :
+        player2_input = str(input("\nPlease select a valid character! :"))
+
+    for i in range(lengh):
+        if name_list[i] == player2_input:
+            player2 = c_list[i]
+
+    while player1.score > 0 and player2.score > 0:
+        print("\n" + "=" * 30)
+        print(f"{player1.name}'s Score: {player1.score}")
+        print(f"{player2.name}'s Score: {player2.score}")
+        print("=" * 30 + "\n")
+
+        player1.assemble_insult(player1.input_insult())
+        print(f"{player1.name}'s insult: {player1.current_insult}")
+
+        damage, _, _ = player1.calculate_damage(player1,player2)
+        print(f"\nDamage dealt to {player2.name}: {damage}\n")
+
+        player2.assemble_insult(player2.input_insult())
+        print(f"{player2.name}'s insult: {player2.current_insult}")
+
+        damage, _, _ = player2.calculate_damage(player2,player1)
+        print(f"\nDamage dealt to {player1.name}: {damage}\n")
+
+    print("\nGame Over!")
+    if player1.score > player2.score:
+        print(f"{player1.name} wins!")
+    elif player2.score > player1.score:
+        print(f"{player2.name} wins!")
+    else:
+        print("It's a draw!")
+
+if __name__ == "__main__":
+    main()
